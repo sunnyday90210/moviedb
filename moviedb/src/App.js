fb55ae476b9290f5174605841f04515f4f01692d
sunnyday90210 <robertjohnwollny@gmail.com>
2018-05-13 19:07:34 -0700

    added keys

---
+++ moviedb/src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  submit = () => {
+    console.log(this.text);
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Welcome text="Welcome to using props and all that" />
+        </header>
+        <p className="App-intro">
+          To get started, edit <code> src / App.js </code> and save to reload.
+        </p>
+        <input type="text" ref={input => (this.text = input)}/>
+        <button onClick={this.submit}>Show value</button>
+      </div>
+    );
+  }
+}
+
+class Welcome extends Component {
+  render() {
+    const { text } = this.props;
+    return <h1 className="App-title">{text}</h1>;
+  }
+}
+
+export default App;
